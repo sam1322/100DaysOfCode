81caec5dbbc2dccb3b565ac7d73d8c88222397e7
sam1322 <sriram.pant5@gmail.com>
2022-04-07 23:44:32 +0530

    updating some files

--- codeforces/bisection.cpp
+++ codeforces/bisection.cpp
@@ -8,7 +8,8 @@ const ld e = 2.71828182845904523536;
 const ld epsilon = 1e-4;
 
 ld f(ld x) {
-	return x * x * x - 5 * x + 1;//for the equation x^3 - 5x + 1 = 0
+	// return x * x * x - 5 * x + 1;//for the equation x^3 - 5x + 1 = 0
+	return x * x * x - 4 * x - 9 ;//for the equation x^3 - 4x - 9 = 0 
 }
 
 void solve() {
@@ -17,6 +18,8 @@ void solve() {
 	if (a > b) {
 		swap(a, b);
 	}
+	cout<<"f("<<a<<") = "<<f(a)<<endl;
+	cout<<"f("<<b<<") = "<<f(b)<<endl;
 	if (f(a) * f(b) >= 0) {
 		cout << "Invalid starting points!" << endl;
 		return;
@@ -31,6 +34,7 @@ void solve() {
 	else {
 		a = c;
 		}
+		cout<<"epoch "<<epoch<<" : "<<c<<endl;
 	}
 	cout << c << endl;
 }
@@ -39,10 +43,22 @@ signed main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout << fixed << setprecision(4);
-	# ifndef ONLINE_JUDGE
-	freopen("in", "r", stdin);
-	freopen("out", "w", stdout);
-	# endif
+	// # ifndef ONLINE_JUDGE
+	// freopen("in", "r", stdin);
+	// freopen("out", "w", stdout);
+	// # endif
 	solve();
 	return 0;
 }
+
+/*
+
+f(x) = x^3 - 4x - 9 = 0 
+
+f(0) < 0 
+f(1) < 0
+f(-1) = -1 + 4 -9 =-6 < 0
+f(2) = 8 - 8 - 9 = -9 
+f(3) = 27 - 
+
+*/
